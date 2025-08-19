ffefd63371180edb83c20824e15277ca0b340e12
Sam Allmon <sallmon@fsc-corp.org>
2025-08-19 15:37:34 -0500

    fixing 500 error

--- pages/api/pages/surveysPageData.js
+++ pages/api/pages/surveysPageData.js
@@ -6,7 +6,7 @@ export default async(req, res) => {
 
     const {db} = await connectToDatabase()
     const user = await db.collection("users").findOne({_id: ObjectId(req.query.userId)})
-    const client = (query.clientId ? await db.collection("users").findOne({_id: ObjectId(req.query.clientId)}) : undefined)
+    const client = (req.query.clientId ? await db.collection("users").findOne({_id: ObjectId(req.query.clientId)}) : undefined)
     const query = { userId: ObjectId(req.query.userId) }
 
     const surveysCursor = await db.collection("lifeAreaSurveys").find( query )
