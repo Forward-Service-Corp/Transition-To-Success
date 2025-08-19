8b7907bf6815958e79cdb19ce4461e39fc5e38bc
Sam Allmon <sallmon@fsc-corp.org>
2025-08-19 15:58:49 -0500

    Added error handling to add a name to a survey if it doesn't have one

--- pages/api/get-client-survey.js
+++ pages/api/get-client-survey.js
@@ -6,8 +6,15 @@ export default async (req, res) => {
 
     const {db} = await connectToDatabase()
     const cursor = await db.collection("lifeAreaSurveys").find({_id: ObjectId(req.query.surveyId)})
-    const records = await cursor.toArray()
+    const survey = await cursor.toArray()
     await cursor.close()
+
+    let records;
+    if (!survey.name) {
+        const client = await db.collection("users").findOne({ id: ObjectId(survey.userId)})
+
+        records = {...survey, name: client.name}
+    }
 
     res.json(records)
 
