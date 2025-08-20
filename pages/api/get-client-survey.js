43ac62555bfecaa0e28ce4ef10dbc4eb9451ca9f
Sam Allmon <sallmon@fsc-corp.org>
2025-08-20 11:46:09 -0500

    removing check for validity entirely, because it keeps crashing.

--- pages/api/get-client-survey.js
+++ pages/api/get-client-survey.js
@@ -1,19 +1,19 @@
-import {connectToDatabase} from "../../lib/dbConnect";
-import {ObjectId} from "mongodb";
+import { connectToDatabase } from "../../lib/dbConnect";
+import { ObjectId } from "mongodb";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
+  const { db } = await connectToDatabase();
+  const survey = await db
+    .collection("lifeAreaSurveys")
+    .find({ _id: ObjectId(req.query.surveyId) });
 
-    const {db} = await connectToDatabase()
-    const survey = await db.collection("lifeAreaSurveys").find({_id: ObjectId(req.query.surveyId)})
+  let records;
+  const client = await db
+    .collection("users")
+    .findOne({ id: ObjectId(survey.userId) });
 
-    let records;
-    if (survey.name === undefined) {
-        const client = await db.collection("users").findOne({ id: ObjectId(survey.userId)})
+  records = { ...survey, name: client.name };
 
-        records = {...survey, name: client.name}
-    }
-
-    res.json(records)
-
-}
+  res.json(records);
+};
