8d4efb0d76f3496c261607343174a31fd822415d
Sam Allmon <sallmon@fsc-corp.org>
2025-08-20 11:38:36 -0500

    chagned the check to explicitly check if it's undefined, not if it's falsy, as falsy was causing a server error

--- pages/api/get-client-survey.js
+++ pages/api/get-client-survey.js
@@ -5,10 +5,10 @@ import {ObjectId} from "mongodb";
 export default async (req, res) => {
 
     const {db} = await connectToDatabase()
-    const survey = await db.collection("lifeAreaSurveys").findOne({_id: ObjectId(req.query.surveyId)})
+    const survey = await db.collection("lifeAreaSurveys").find({_id: ObjectId(req.query.surveyId)})
 
     let records;
-    if (!survey.name) {
+    if (survey.name === undefined) {
         const client = await db.collection("users").findOne({ id: ObjectId(survey.userId)})
 
         records = {...survey, name: client.name}
