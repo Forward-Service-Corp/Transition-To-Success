9fececbddbc7a125c12f89efe1542814ac9a9147
sallmonFSC <sallmon@fsc-corp.org>
2025-10-08 17:35:26 -0500

    bugfix for useAutoLogout. added session refetching every 30 seconds, and put all of the logout code into an if (userLevel === 'client') block, instead of using return.

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import {SessionProvider} from "next-auth/react";
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
-      <SessionProvider session={session}>
+      <SessionProvider session={session} refetchInterval={30}>
         <Component {...pageProps} />
       </SessionProvider>
     );
