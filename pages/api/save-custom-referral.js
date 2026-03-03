b5720fc4336f0f8e5b2732a0a85e913d71100867
sallmonFSC <sallmon@fsc-corp.org>
2026-03-03 12:03:18 -0600

    Found a bug in the custom-referral code, fixing to see if that caused the 405

--- pages/api/save-custom-referral.js
+++ pages/api/save-custom-referral.js
@@ -5,27 +5,27 @@ import {ObjectId} from "mongodb";
 export default async(req, res) => {
     const today = new Date()
     const record = {
-        surveyId: req.body.surveyId,
-        userId: ObjectId(req.body.userId),
-        dream: req.body.dream,
-        domain: req.body.domain,
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        hours: req.body.hours,
-        requirements: req.body.requirements,
-        url: req.body.url,
-        contact: req.body.contact,
-        needs: req.body.needs,
-        isCustom: true,
-        createdDate: date
-    }
+      surveyId: req.body.surveyId,
+      userId: ObjectId(req.body.userId),
+      dream: req.body.dream,
+      domain: req.body.domain,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      hours: req.body.hours,
+      requirements: req.body.requirements,
+      url: req.body.url,
+      contact: req.body.contact,
+      needs: req.body.needs,
+      isCustom: true,
+      createdDate: today,
+    };
 
-    const {db} = await connectToDatabase()
-    const user = await db
-        .collection("customReferrals")
-        .insertOne(record)
+    const { db } = await connectToDatabase();
+    const user = await db.collection("customReferrals").insertOne(record);
 
-    res.json(user)
+    return res.json(user);
+  }
 
-}
+  return res.status(405).json({ error: "Method not allowed" });
+};
