71d27f50c0fec864a5e2762c3c131fee294a3967
sallmonFSC <sallmon@fsc-corp.org>
2026-05-21 12:12:15 -0500

    forgot to remove the reset from save when separating

--- pages/add-new-referral.js
+++ pages/add-new-referral.js
@@ -74,7 +74,6 @@ export default function AddNewReferral({ pageDataJson }) {
       setSaving(false);
     });
     if (response.ok) {
-      resetReferral();
       setMessage("Success!");
       setSaving(false);
       setTimeout(() => {
