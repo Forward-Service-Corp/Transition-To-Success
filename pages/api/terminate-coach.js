3d25125fa0dcc4b822dd8ad66789da33d39b51b6
sallmonFSC <sallmon@fsc-corp.org>
2026-05-28 10:20:42 -0500

    Fixing termination logic to check based on both key and _id, since the data is bad.

--- pages/api/terminate-coach.js
+++ pages/api/terminate-coach.js
@@ -1,49 +1,77 @@
-import {connectToDatabase} from "../../lib/dbConnect";
-import {ObjectId} from "mongodb";
+import { connectToDatabase } from "../../lib/dbConnect";
+import { ObjectId } from "mongodb";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
-    const { coachId } = req.query;
-    const {db} = await connectToDatabase();
-    const usersCollection = db.collection('users');
-    const coachObjectId = new ObjectId(coachId); // Assuming coachId is an ObjectId
+  const { coachId } = req.query;
+  const { db } = await connectToDatabase();
+  const usersCollection = db.collection("users");
+  const coachObjectId = new ObjectId(coachId); // Assuming coachId is an ObjectId
 
-    // Update to set terminationDate in the matching coach array object
-    const updateResult = await usersCollection.updateMany(
-        {
-            coach: { $elemMatch: { key: coachObjectId } },
+  // Update to set terminationDate in the matching coach array object
+  const updateResult = await usersCollection.updateMany(
+    {
+      coach: {
+        $elemMatch: {
+          $or: [
+            {
+              _id: coachObjectId,
+            },
+            {
+              key: coachObjectId,
+            },
+          ],
         },
+      },
+    },
+    {
+      $set: { "coach.$[elem].terminationDate": new Date() },
+    },
+    {
+      arrayFilters: [
         {
-            $set: { 'coach.$[elem].terminationDate': new Date() }
+          $or: [{ "elem._id": coachObjectId }, { "elem.key": coachObjectId }],
         },
-        {
-            arrayFilters: [{ 'elem.key': coachObjectId }]
-        }
-    );
+      ],
+    },
+  );
 
-    // Check if documents were modified
-    if (updateResult.modifiedCount > 0) {
-
-        // Retrieve the updated documents
-        const updatedDocuments = await usersCollection
-            .find({
-                coach: { $elemMatch: { key: coachObjectId, terminationDate: { $exists: true } } }
-            })
-            .toArray();
-        res.status(200).json({
-            message: `Coach removed from ${updateResult.modifiedCount} clients.`,
-            matchedCount: updateResult.matchedCount,
-            modifiedCount: updateResult.modifiedCount,
-            updatedDocuments
-        });
-    }else {
-        res.status(200).json({
-            message: 'Coach not removed from any clients.',
-            matchedCount: updateResult.matchedCount,
-            modifiedCount: updateResult.modifiedCount,
-            updatedDocuments: [],
-        });
-    }
-
-
-}
+  // Check if documents were modified
+  if (updateResult.modifiedCount > 0) {
+    // Retrieve the updated documents
+    const updatedDocuments = await usersCollection
+      .find({
+        coach: {
+          $elemMatch: {
+            $and: [
+              {
+                $or: [
+                  {
+                    key: coachObjectId,
+                  },
+                  {
+                    _id: coachObjectId,
+                  },
+                ],
+              },
+              { terminationDate: { $exists: true } },
+            ],
+          },
+        },
+      })
+      .toArray();
+    res.status(200).json({
+      message: `Coach removed from ${updateResult.modifiedCount} clients.`,
+      matchedCount: updateResult.matchedCount,
+      modifiedCount: updateResult.modifiedCount,
+      updatedDocuments,
+    });
+  } else {
+    res.status(200).json({
+      message: "Coach not removed from any clients.",
+      matchedCount: updateResult.matchedCount,
+      modifiedCount: updateResult.modifiedCount,
+      updatedDocuments: [],
+    });
+  }
+};
